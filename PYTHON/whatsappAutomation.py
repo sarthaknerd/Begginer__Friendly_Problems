99d2898815f99060c63ca3ca5d690a36aee30536
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2022-10-16 23:41:09 +0530

    WhatsApp Automation

---
+++ PYTHON/whatsappAutomation.py
@@ -0,0 +1,14 @@
+
+import pywhatkit
+ 
+
+try:
+   
+
+  pywhatkit.sendwhatmsg("+91xxxxxxxxxx","Hello from Sarthak i contributed in Begginer__Friendly_Problems",22, 28)
+  print("Successfully Sent!")
+ 
+except:
+   
+
+  print("An Unexpected Error!")
